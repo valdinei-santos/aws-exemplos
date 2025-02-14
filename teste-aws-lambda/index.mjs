9f2e21043dbda3904449c60665f8240acd2b93df
Valdinei Santos <valdinei.vs@gmail.com>
2025-02-14 08:24:22 -0300

    Alterações para usar o S3 via SDK da AWS

--- teste-aws-lambda/index.mjs
+++ teste-aws-lambda/index.mjs
@@ -1,13 +1,29 @@
 import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
+
+const s3Client = new S3({ region: 'us-east-1' });
 
 export const handler = async (event) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(event),
-      headers: {
-        "content-type": "application/json",
-      }
-    };
-    log('event: ', JSON.stringify(event));
-    return response;
+    // const response = {
+    //   statusCode: 200,
+    //   body: JSON.stringify(event),
+    //   headers: {
+    //     "content-type": "application/json",
+    //   }
+    // };
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({ Bucket, Key });
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+      log('Objeto muito grande');
+      return 'Objeto muito grande';
+    }
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
+
+    //  log('event: ', JSON.stringify(event));
+    //  return response;
   };
